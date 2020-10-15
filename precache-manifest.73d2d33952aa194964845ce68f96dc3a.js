02edc9e28db532fc41b5a8fae5284b2da5fedc01
Volodymyr Pohoriliak <volodymyr.pohoriliakv@loungelogic.tv>
2020-10-16 01:10:06 +0300

    Updates

---
+++ precache-manifest.73d2d33952aa194964845ce68f96dc3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03cd731cfc11626187bee0a94d9af40a",
+    "url": "/ekosfera.github.io/index.html"
+  },
+  {
+    "revision": "6561434578f3822491f0",
+    "url": "/ekosfera.github.io/static/css/main.8e578ee8.chunk.css"
+  },
+  {
+    "revision": "c7dd4ec25bc6d18de0c4",
+    "url": "/ekosfera.github.io/static/js/2.7c432222.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/ekosfera.github.io/static/js/2.7c432222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6561434578f3822491f0",
+    "url": "/ekosfera.github.io/static/js/main.f4cb956b.chunk.js"
+  },
+  {
+    "revision": "7ebde046b49fe17f74c9",
+    "url": "/ekosfera.github.io/static/js/runtime-main.e8798083.js"
+  },
+  {
+    "revision": "d29a82cfb217082cded0ae1b3b24eb0b",
+    "url": "/ekosfera.github.io/static/media/placeholder.d29a82cf.png"
+  }
+]);
